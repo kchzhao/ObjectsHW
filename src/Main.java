075f4c9e5cc75cf987e05d972918a89b1970d854
kchzhao <kamila.chzhao@gmail.com>
2022-10-30 06:47:24 -0400

    Initial commit to branch1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //HW
         Book book1 = new Book("Martin Eden","Jack London", 1909);
         Book book2 = new Book("War and Peace","Leo Tolstoy",1865);
         Author author1 = new Author("Jack","London");
