929acfdd10d19cfbbc4f770e33fe0148d1f84b0f
kchzhao <kamila.chzhao@gmail.com>
2022-10-30 21:02:48 -0400

    Correct commit to master

--- src/Author.java
+++ src/Author.java
@@ -1,19 +1,3 @@
 public class Author {
-    String firstName;
-    String lastName;
-
-    public Author(String firstName, String lastName){
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName(){
-        return firstName;
-    }
-
-    public String getLastName(){
-        return lastName;
-    }
-
 
 }
