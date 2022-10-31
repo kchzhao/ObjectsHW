8a592a7403aa344b70bd7de1fadb6cd612375ad1
kchzhao <kamila.chzhao@gmail.com>
2022-10-30 21:10:59 -0400

    third commit to master

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,3 @@
 public class Main {
-    public static void main(String[] args) {
-        //HW
-        Book book1 = new Book("Martin Eden", "Jack London", 1909);
-        Book book2 = new Book("War and Peace", "Leo Tolstoy", 1865);
-        Author author1 = new Author("Jack", "London");
-        Author author2 = new Author("Leo", "Tolstoy");
-        book1.setYear(2022);
-    }
+
 }
